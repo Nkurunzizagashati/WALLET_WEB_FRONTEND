998330ad4423642db7954f9dd9067910f9a5067f
Fabrice <f.nkurunziz@alustudent.com>
2025-01-16 19:17:24 +0200

    define the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
+import CategoryPage from './pages/CategoriesPage';
+import BudgetPage from './pages/BudgetPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-	return <HomePage />;
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <HomePage />,
+		},
+		{
+			path: '/dashboard',
+			element: <DashboardPage />,
+		},
+		{
+			path: '/categories',
+			element: <CategoryPage />,
+		},
+		{
+			path: '/budget',
+			element: <BudgetPage />,
+		},
+	]);
+
+	return <RouterProvider router={router} />;
 }
 
 export default App;
