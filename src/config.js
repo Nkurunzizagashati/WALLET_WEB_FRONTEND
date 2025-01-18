20c9d2a4cdd355d1412f4f8d859f65e42fe6ab7d
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 14:30:00 +0200

    update the backend url to use the one on railway

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const backendUrl =
+	'https://transactionsmanagerbackend-production-efd7.up.railway.app/api/';
+const backendUrl2 =
 	'https://transactions-manager-backend.onrender.com/api';
 
 export { backendUrl };
