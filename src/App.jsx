06c477becc7ede7159ad13db285cc1edc6768318
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 14:30:00 +0200

    add functionality for fetching incomes and expenses on the first reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,30 @@
 import './App.css';
-import HomePage from './pages/HomePage';
-import BudgetPage from './pages/BudgetPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import TransactionsPage from './pages/TransactionsPage';
+import { useEffect } from 'react';
+import BudgetPage from './pages/BudgetPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
 import ReportsPage from './pages/ReportsPage';
 import SettingsPage from './pages/SettingsPage';
-import RegistrationPage from './pages/RegistrationPage';
-import LoginPage from './pages/LoginPage';
-import { useEffect } from 'react';
-import {
-	fetchAccounts,
-	fetchCategories,
-	fetchTransactions,
-} from './redux/actions';
-import {
-	fetchCategoriesSuccess,
-	fetchCategoriesFailure,
-	fetchCategoriesStart,
-} from './redux/categorySlice';
+import TransactionsPage from './pages/TransactionsPage';
+
 import { useDispatch, useSelector } from 'react-redux';
+
 import {
-	fetchTransactionsFailure,
-	fetchTransactionsStart,
-	fetchTransactionsSuccess,
-} from './redux/transactionSlice';
-import {
-	fetchAccountsFailure,
-	fetchAccountsStart,
-	fetchAccountsSuccess,
-} from './redux/accountSlice';
+	callFetchAccountsFunction,
+	callFetchCategoriesFunction,
+	callFetchTransactionsFunction,
+} from './fetch';
 
 function App() {
 	const dispatch = useDispatch();
 	const { user } = useSelector((state) => state.auth);
 
 	useEffect(() => {
-		const callFetchCategoriesFunction = async () => {
-			try {
-				fetchCategoriesStart();
-				const data = await fetchCategories();
-				dispatch(fetchCategoriesSuccess(data.categories));
-			} catch (error) {
-				dispatch(fetchCategoriesFailure(error.message));
-			}
-		};
-
-		const callFetchTransactionsFunction = async () => {
-			try {
-				dispatch(fetchTransactionsStart());
-				const data = await fetchTransactions();
-
-				dispatch(fetchTransactionsSuccess(data.transactions));
-			} catch (error) {
-				dispatch(fetchTransactionsFailure(error.message));
-			}
-		};
-
-		const callFetchAccountsFunction = async () => {
-			try {
-				dispatch(fetchAccountsStart());
-				const data = await fetchAccounts();
-				dispatch(fetchAccountsSuccess(data.accounts));
-				console.log(data.accounts);
-			} catch (error) {
-				dispatch(fetchAccountsFailure(error.message));
-			}
-		};
-
-		callFetchCategoriesFunction();
-		callFetchTransactionsFunction();
-		callFetchAccountsFunction();
+		callFetchCategoriesFunction(dispatch);
+		callFetchTransactionsFunction(dispatch);
+		callFetchAccountsFunction(dispatch);
 	}, [dispatch, user]);
 
 	const router = createBrowserRouter([
