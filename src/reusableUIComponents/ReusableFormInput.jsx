810edbcf522cd790bc522e5b6b4d4d264fe735d7
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 22:23:22 +0200

    create a reusable form input ui component

--- src/reusableUIComponents/ReusableFormInput.jsx
+++ src/reusableUIComponents/ReusableFormInput.jsx
@@ -1,39 +1,34 @@
 import PropTypes from 'prop-types';
 
 const ReusableFormInput = ({
-	formData,
 	className = '',
 	onChange,
 	type = 'text',
 	name,
+	value,
+	...rest
 }) => {
 	return (
-		<div className="mb-4">
-			<label
-				className="block text-sm font-medium mb-1"
-				htmlFor={name}
-			>
-				{name}
-			</label>
-			<input
-				id={name}
-				type={type}
-				name={name}
-				value={formData[name] || ''}
-				onChange={onChange}
-				className={`${className} w-full px-3 py-2 border rounded border-gray-300`}
-			/>
-		</div>
+		<input
+			id={name}
+			type={type}
+			name={name}
+			value={value}
+			onChange={onChange}
+			className={`${className} w-full px-3 py-2 border rounded border-gray-300`}
+			{...rest}
+		/>
 	);
 };
 
 // Validate props
 ReusableFormInput.propTypes = {
-	formData: PropTypes.object.isRequired,
 	className: PropTypes.string,
 	onChange: PropTypes.func.isRequired,
 	name: PropTypes.string.isRequired,
 	type: PropTypes.string.isRequired,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+		.isRequired,
 };
 
 export default ReusableFormInput;
