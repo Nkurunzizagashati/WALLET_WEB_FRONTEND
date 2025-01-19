3127282c88e84bfb75299736f8c00d3fbd638476
Fabrice <f.nkurunziz@alustudent.com>
2025-01-19 14:30:00 +0200

    register toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
+		<Toaster position="top-right" reverseOrder={false} />
 		<App />
 	</Provider>
 );
