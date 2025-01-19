96b8a8f152cd5f647ae0cb47021efe78602adcf6
Fabrice <f.nkurunziz@alustudent.com>
2025-01-19 11:14:35 +0200

    add the logic to enable only login or register links in case you are not logged in

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,36 +1,103 @@
 import PropTypes from 'prop-types';
 import { LuLayoutDashboard } from 'react-icons/lu';
-import { MdHomeFilled } from 'react-icons/md';
+import { TbTransactionDollar } from 'react-icons/tb';
+import { TbReportMoney } from 'react-icons/tb';
+import { SiActualbudget } from 'react-icons/si';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { MdLogin } from 'react-icons/md';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Aside = ({ className }) => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const authToken = localStorage.getItem('authToken');
+
+	useEffect(() => {
+		if (
+			!authToken &&
+			location.pathname !== '/login' &&
+			location.pathname !== '/register'
+		) {
+			navigate('/login');
+		}
+	}, [authToken, navigate, location.pathname]);
+
 	const navItems = [
-		{
-			icon: <MdHomeFilled className="text-3xl cursor-pointer" />,
-			label: 'Home',
-		},
 		{
 			icon: (
 				<LuLayoutDashboard className="text-3xl cursor-pointer" />
 			),
 			label: 'Dashboard',
+			linkPath: '/',
+		},
+		{
+			icon: <TbReportMoney className="text-3xl cursor-pointer" />,
+			label: 'Reports',
+			linkPath: '/reports',
+		},
+		{
+			icon: (
+				<TbTransactionDollar className="text-3xl cursor-pointer" />
+			),
+			label: 'Transactions',
+			linkPath: '/transactions',
+		},
+		{
+			icon: (
+				<SiActualbudget className="text-3xl cursor-pointer" />
+			),
+			label: 'Budget',
+			linkPath: '/budget',
+		},
+		{
+			icon: <MdLogin className="text-3xl cursor-pointer" />,
+			label: 'Login',
+			linkPath: '/login',
 		},
 	];
 
 	return (
 		<aside
-			className={`${className} text-dark-text text-lg h-full fixed left-0 py-3 px-2 bg-light-gray`}
+			className={`${className} text-dark-text text-lg h-full fixed left-0 py-3 px-2 pb-10 bg-light-gray`}
 		>
-			<ul className="flex flex-col gap-4">
+			<div className="flex flex-col gap-4 h-[80%]">
 				{navItems.map((navItem, index) => (
-					<li
+					<NavLink
+						to={navItem.linkPath}
 						key={index}
-						className="flex gap-4 cursor-pointer items-center w-full py-2 border hover:border-silver-color rounded-md"
+						className={({ isActive }) =>
+							`${
+								isActive
+									? 'bg-primary text-light-gray'
+									: ''
+							} flex gap-4 cursor-pointer items-center w-full py-2 hover:bg-primary hover:text-light-gray rounded-xl ${
+								authToken ? '' : 'pointer-events-none'
+							} ${
+								navItem.linkPath === '/login'
+									? 'mt-6 pointer-events-auto'
+									: ''
+							} `
+						}
 					>
 						{navItem.icon}
 						<p className="">{navItem.label}</p>
-					</li>
+					</NavLink>
 				))}
-			</ul>
+			</div>
+
+			<NavLink
+				to="/settings"
+				className={({ isActive }) =>
+					`${
+						isActive ? 'bg-primary text-light-gray' : ''
+					} flex gap-4 cursor-pointer items-center w-full py-2 hover:bg-primary hover:text-light-gray rounded-xl`
+				}
+			>
+				<IoSettingsOutline className="text-3xl" />
+				<p className="">Settings</p>
+			</NavLink>
 		</aside>
 	);
 };
