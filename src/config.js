aabdd8a5305531294c1c8bed2fc488b1dfd92c75
Fabrice <f.nkurunziz@alustudent.com>
2025-01-20 00:39:01 +0200

    update the backend url to the deployed one

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const backendUrl = 'http://localhost:3002/api';
+const backendUrl1 = 'http://localhost:3002/api';
+const backendUrl = 'https://web-wallet-backend-62qx.onrender.com/api';
 
 export { backendUrl };
