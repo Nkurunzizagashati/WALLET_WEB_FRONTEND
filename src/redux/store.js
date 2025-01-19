c652f4d319572e6b38f0c10dc3065c713899594b
Fabrice <f.nkurunziz@alustudent.com>
2025-01-19 23:55:43 +0200

    create the store for accessing all redux data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import transactionsReducer from './transactionSlice';
 import authReducer from './authSlice';
+import categoriesReducer from './categorySlice';
+import accountsReducer from './accountSlice';
+import navLinkTextsReducer from './navLinkTextsSlice';
 
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		transactions: transactionsReducer,
+		categories: categoriesReducer,
+		accounts: accountsReducer,
+		navLinkTexts: navLinkTextsReducer,
 	},
 });
 
