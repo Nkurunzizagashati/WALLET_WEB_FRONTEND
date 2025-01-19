67b8ea65e78c0c2896ad5dfa709c1260fb607909
Fabrice <f.nkurunziz@alustudent.com>
2025-01-19 23:46:14 +0200

    create the home page which displays the dashboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
+import { useSelector } from 'react-redux';
 import Aside from '../components/Aside';
 import Navbar from '../components/Navbar';
 import MainPage from './MainPage';
 
 const HomePage = () => {
+	const { showNavLinkTexts } = useSelector(
+		(state) => state.navLinkTexts
+	);
 	return (
-		<div className="h-screen ">
+		<div className="">
 			<Navbar className="max-h-[10%]" />
-			<div className="h-screen mt-[5.1%] ">
-				<Aside className="top-[11%]" />
-				<MainPage className="ml-[15.2%] text-red-700 h-full bg-light-gray" />
+			<div className=" mt-[5.1%] ">
+				<Aside
+					className={`${
+						showNavLinkTexts ? 'w-[15%]' : 'w-[5%]'
+					}`}
+				/>
+				<MainPage
+					className={`${
+						showNavLinkTexts ? 'ml-[15.2%]' : 'ml-[5.2%]'
+					}`}
+				/>
 			</div>
 		</div>
 	);
